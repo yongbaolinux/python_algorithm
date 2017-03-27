64139ae4b74eb121b9c0dfe3788f6d4c7a2d2b76
root <root@localhost.localdomain>
2017-03-27 18:26:40 +0800

    123

--- Simulated Annealing_Genetic Algorithm.py
+++ Simulated Annealing_Genetic Algorithm.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+## 模拟退火算法和遗传算法
 
 import random
 import time
+import math
 '''##使用模拟退火算法求一元函数极值
 def F(x):
     return 6*x**7+8*x**6+7*x**3+5*x**2-100*x
@@ -31,7 +33,6 @@ def simulatedAnnealing(startX,endX):
 
 print simulatedAnnealing(0,100)'''
 
-'''模拟退火算法设计行程安排'''
 ##航班信息
 planes = {}
 people = [('zhangsan','BOS'),('lisi','DAL'),('wangwu','CAK'),('wanger','MIA')]
@@ -137,6 +138,8 @@ def hillClimb(people):
             if i < persons:
                 # 在原基础上正负两次波动
                 for k in range(-1,2,2):
+                    if (wang[i]+k < 0) or (wang[i]+k > 9):
+                        continue
                     wang_ = wang[0:i] + [wang[i]+k] + wang[i+1:]
                     cost_ = calSchedule(people,wang_,fan)
                     if(cost_ < bestCost):
@@ -145,7 +148,10 @@ def hillClimb(people):
                         bestCost = cost_
             #修改返程 安排
             else:
+                # 在原基础上正负两次波动 如果小于0 退出本次循环
                 for k in range(-1,2,2):
+                    if (fan[i-persons]+k < 0) or (fan[i-persons]+k > 9):
+                        continue
                     fan_ = fan[0:i-persons] + [fan[i-persons]+k] + fan[i-persons+1:]
                     cost_ = calSchedule(people,wang,fan_)
                     if (cost_ < bestCost):
@@ -159,8 +165,72 @@ def hillClimb(people):
             fan = bestFan
             cost = bestCost
 
-wang,fan = hillClimb(people)
+#wang,fan = hillClimb(people)
+#printSchedule(people,wang,fan)
+
+
+'''模拟退火算法 退火公式 random.random()<pow(math.e , -(新的估值-旧的估值)/T)'''
+def simulatedAnnealing(people,T=10000.0,cool=0.95,step=1):
+    persons = len(people)
+    wang = [random.randint(0,9) for i in range(persons)]
+    fan = [random.randint(0,9) for i in range(persons)]
+    cost = calSchedule(people,wang,fan)
+    while T > 0.1:
+        ##pos-变化位置 step-变化方向
+        pos = random.randint(0,persons*2-1)
+        step_ = random.randint(-step, step)
+        if pos >= persons:
+            pos -= persons
+            if (fan[pos] + step_ < 0) or (fan[pos] + step_ > 9):
+                continue
+            fan_new = fan[0:pos] + [fan[pos] + step_] + fan[pos+1:]
+            cost_ = calSchedule(people,wang,fan_new)
+            #接收更优解或者可能接收更差解
+            if((cost_ < cost) or (random.random()<pow(math.e , -(cost_ - cost)/T))):
+                cost = cost_
+                fan = fan_new
+        else:
+            if (wang[pos] + step_ < 0) or (wang[pos] + step_ > 9):
+                continue
+            wang_new = wang[0:pos] + [wang[pos] + step_] + wang[pos+1:]
+            cost_ = calSchedule(people,wang_new,fan)
+            #接收更优解或者可能接收更差解
+            if((cost_ < cost) or (random.random()<pow(math.e , -(cost_ - cost)/T))):
+                cost = cost_
+                wang = wang_new
+        T = T * cool
+
+    return wang,fan
+
+wang,fan = simulatedAnnealing(people)
 printSchedule(people,wang,fan)
 
+'''遗传算法'''
+## 变化 step 变化跨度
+def change(wang,fan,step=1):
+    wang_ = wang
+    fan_ = fan
+    len_ = len(wang)
+    assert len_ == len(fan)
+    i = random.randint(0,len_*2-1)
+    if i < len_:
+        if(wang[i] + step) <= 9:
+            wang_ = wang[0:i] + [wang[i] + step] + wang[i+1:]
+        elif(wang[i] - step) >= 0:
+            wang_ = wang[0:i] + [wang[i] - step] + wang[i+1:]
+    else:
+        if (fan[i] + step) <= 9:
+            fan_ = fan[0:i] + [fan[i] + step] + fan[i+1:]
+        elif (fan[i] - step) >= 0:
+            fan_ = fan[0:i] + [fan[i] - step] + fan[i+1:]
+    return wang_,fan_
 
-'''模拟退火算法'''
+##交叉 pos 交叉点位置
+def cross(wang,fan,wang2,fan2,pos=1):
+    assert pos>0 and pos<len(wang)-1
+    assert len(wang) == len(fan) == len(wang2) == len(fan2)
+    return (wang[0:pos] + wang2[pos+1:]),(fan[0:pos]+fan2[pos+1:])
+
+##popSize 初始种群大小 
+##change
+def geneticAlgorithm(popSize=100,changeProp=0.3,top=0.2,generation=100):
